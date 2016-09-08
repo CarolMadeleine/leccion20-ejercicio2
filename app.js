e27aa94074db082cdd63ade6a1187e731c9b3f45
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-08 10:27:59 -0500

    Modificación

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+var feature = "cloures";
 (function(){
-	var feature = "cloures";
 	if (typeof feature === "undefined"){
-		var feature = "callbacks";
+		feature = "callbacks";
 		console.log("JS coders love its " + feature);
 	} else {
 		console.log("JS developers love its " + feature);
